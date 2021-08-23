6e8dad8944b502d426d0e0db2f51d40c9a0229fa
Alex Silva Z <a.silvazapata@gmail.com>
2021-08-23 23:32:50 +0000

    validations for cvc and card length

--- main.js
+++ main.js
@@ -11,10 +11,10 @@
       .forEach(function (form) {
         form.addEventListener('submit', function (event) {
           if (!form.checkValidity()) {
+            
             event.preventDefault()
             event.stopPropagation()
           }
-  
           form.classList.add('was-validated')
         }, false)
       })
